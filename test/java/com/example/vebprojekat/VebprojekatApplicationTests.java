1cff8621fb1261f19f9890c05a663e420e5c9510
Isailovic11 <71615605+Isailovic11@users.noreply.github.com>
2020-09-21 06:53:33 +0200

    Add files via upload

---
+++ test/java/com/example/vebprojekat/VebprojekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vebprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebprojekatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
